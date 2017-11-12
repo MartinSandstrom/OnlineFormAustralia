78b3b4dce2922681cd390c3709d59358703e2f63
Martin Sandström <martinsandstroms@gmail.com>
2017-11-12 18:57:32 +1100

    CHORE: Cleanup server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html');
 });
 
-
-//Something like this might be used if we decide to not go with heroku proxy :)
+//Something like this might be used if we decide to not go with CORS any where proxy :)
 app.get('/data', (req, res) => {
+    //TODO get request params.
     let URL = 'https://digitalapi.auspost.com.au/postcode/search?q=Melbourne&state=VIC';
     var headers = {
-        'Auth-Key': 'ad52e4b6-5e22-42ba-bdbb-1882b9dacc77',
-        'Content-Type': 'json'
+        'Auth-Key': 'ad52e4b6-5e22-42ba-bdbb-1882b9dacc77'
     };
     request.get({
         url: URL,
+        json: true,
         headers: headers
     }, (e, r, body) => {
 		res.send(JSON.stringify(body));
